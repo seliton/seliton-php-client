c1f0a1a31879413420f8d194319bb8a49e28475c
Alexander Makhaev <alexander.makhaev@gmail.com>
2016-01-02 21:08:15 +1000

    Order resource added

---
+++ tests/OrderTestCase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Seliton\Client;
+
+class OrderTestCase extends \PHPUnit_Framework_TestCase
+{
+	public function testRetrieve()
+	{
+		$orderRetrieved = Order::retrieve(2048);
+
+		$this->assertEquals('Flat Rate', $orderRetrieved->shippingModuleName);
+		$this->assertEquals('Collect on delivery', $orderRetrieved->paymentModuleName);
+	}
+
+	public function testAll()
+	{
+		list ($orders, $count) = Order::all(array (
+			'limit' => 2,
+			'offset' => 1,
+			'fields' => 'orderId,orderPaymentModuleName'
+		));
+
+		$this->assertEquals(286, $count);
+		$this->assertEquals(2, count($orders));
+
+		for ($j = 0; $j < 2; $j++) {
+			$this->assertEquals('Collect on delivery', $orders[$j]->paymentModuleName);
+			$this->assertTrue(!isset($orders[$j]->shippingModuleName));
+		}
+	}
+}
