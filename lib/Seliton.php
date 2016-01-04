c70e6c3182456116d484f62b6cce1791f3e9c8ac
Alexander Makhaev <alexander.makhaev@gmail.com>
2016-01-05 02:11:23 +1000

    Attribute, Brand, Category, Customer, Order and Product resources support added to Seliton's factory methods

--- lib/Seliton.php
+++ lib/Seliton.php
@@ -13,9 +13,39 @@ class Seliton {
 		return new Seliton($apiUrl);
 	}
 
+	public function attribute()
+	{
+		return new Attribute($this->apiUrl);
+	}
+
+	public function brand()
+	{
+		return new Brand($this->apiUrl);
+	}
+
+	public function category()
+	{
+		return new Category($this->apiUrl);
+	}
+
+	public function customer()
+	{
+		return new Customer($this->apiUrl);
+	}
+
+	public function order()
+	{
+		return new Order($this->apiUrl);
+	}
+
 	public function page()
 	{
 		return new Page($this->apiUrl);
+	}
+
+	public function product()
+	{
+		return new Product($this->apiUrl);
 	}
 
 	public function __construct($apiUrl)
