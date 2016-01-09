721932c70153e1886d3ecb654ffec01419a9af61
Alexander Makhaev <alexander.makhaev@gmail.com>
2016-01-10 03:52:00 +1000

    init.php updated

--- init.php
+++ init.php
@@ -2,14 +2,15 @@
 
 require_once dirname(__FILE__).'/lib/Seliton.php';
 
-require_once dirname(__FILE__).'/lib/Resource.php';
+require_once dirname(__FILE__).'/lib/Resource/Resource.php';
 
-require_once dirname(__FILE__).'/lib/Attribute.php';
-require_once dirname(__FILE__).'/lib/Brand.php';
-require_once dirname(__FILE__).'/lib/Category.php';
-require_once dirname(__FILE__).'/lib/Customer.php';
-require_once dirname(__FILE__).'/lib/Order.php';
-require_once dirname(__FILE__).'/lib/Page.php';
-require_once dirname(__FILE__).'/lib/Product.php';
+require_once dirname(__FILE__).'/lib/Resource/Attribute.php';
+require_once dirname(__FILE__).'/lib/Resource/Brand.php';
+require_once dirname(__FILE__).'/lib/Resource/Category.php';
+require_once dirname(__FILE__).'/lib/Resource/Customer.php';
+require_once dirname(__FILE__).'/lib/Resource/Order.php';
+require_once dirname(__FILE__).'/lib/Resource/Page.php';
+require_once dirname(__FILE__).'/lib/Resource/Product.php';
 
-require_once dirname(__FILE__).'/lib/HttpClient.php';
+require_once dirname(__FILE__).'/lib/HttpClient/HttpClient.php';
+require_once dirname(__FILE__).'/lib/HttpClient/Enum/HttpMethod.php';
